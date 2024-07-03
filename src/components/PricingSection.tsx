8c8bebcf15bb4284710044ef9fcfe85e11918831
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-07-03 15:25:13 +0530

    ssr

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -1,13 +1,17 @@
 import PricingCard from './PricingCard';
 
-export default function PricingSection() {
+type PricingSectionProps = {
+  prices: string[];
+};
+
+export default function PricingSection({ prices }: PricingSectionProps) {
   return (
     <section className="container mx-auto space-y-2">
       <h1 className="text-center text-4xl font-medium">Pricing</h1>
       <div className="grid grid-cols-1 gap-10 sm:grid-cols-2">
         <PricingCard
           plan="Monthly"
-          price="$99"
+          price={prices[0]}
           features={[
             '10 users included',
             '2 GB of storage',
@@ -17,7 +21,7 @@ export default function PricingSection() {
         />
         <PricingCard
           plan="Annually"
-          price="$999"
+          price={prices[1]}
           features={[
             '20 users included',
             '5 GB of storage',
--- src/pages/pages-router/ssr.tsx
+++ src/pages/pages-router/ssr.tsx
@@ -1,10 +1,28 @@
+import { GetServerSideProps } from 'next';
 import PricingSection from '@/components/PricingSection';
 
-export default function PagesRouterSSR() {
+export const getServerSideProps = (async (context) => {
+  const IP = context.req.headers['x-forwarded-for'] || 'unknown';
+  const req = await fetch(`https://api.country.is/${IP}`);
+  const data = await req.json();
+
+  const PRICES: Record<string, string[]> = {
+    IN: ['₹8,000', '₹80,000'],
+    DEFAULT: ['$99', '$999'],
+  };
+
+  return {
+    props: {
+      prices: PRICES[data.country] || PRICES['DEFAULT'],
+    },
+  };
+}) satisfies GetServerSideProps;
+
+export default function PagesRouterSSR({ prices }: { prices: string[] }) {
   return (
     <div className="container mx-auto px-5">
       <p className="text-xl font-bold">PagesRouterSSR</p>
-      <PricingSection />
+      <PricingSection prices={prices} />
     </div>
   );
 }
